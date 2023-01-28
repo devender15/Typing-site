84cc2afb616a18c7d320e141b81a45a6069f0c27
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-01-28 20:33:27 +0530

    final build

--- api/settings.py
+++ api/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$-gq&l7k1-75yngn3@sv8axye=ndqlu7e(^ah*z6imr=8)6ud6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -24,10 +24,10 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:8000'
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:8000'
 ]
 
 
